7d33ed97fd62c44dd2c30d55cab8384e6ebfc45e
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-01-24 15:08:11 +0300

    changed naming of input_time variable

--- tasks/5_sun_angle.py
+++ tasks/5_sun_angle.py
@@ -1,6 +1,6 @@
-inputTime = input()
-hours = int(inputTime.split(":")[0])
-minutes = int(inputTime.split(":")[1])
+input_time = input()
+hours = int(input_time.split(":")[0])
+minutes = int(input_time.split(":")[1])
 start_hours = 6
 hour_degrees_interval = 15
 minute_degrees_interval = 0.25
