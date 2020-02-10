07b7fef4c3f8aa31222115c45471dc8cca5bae38
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-02-10 17:46:39 +0300

    tasks haven't done else!!!

--- homework_2/9_tic_tac_toe_universal.py
+++ homework_2/9_tic_tac_toe_universal.py
@@ -1,43 +1,64 @@
 def determine_winner(char, game_matrix):
-    winner_count = len(game_matrix)
-    if len(game_matrix) > len(game_matrix[0]):
-        winner_count = len(game_matrix[0])
-
-    check_strings = []
-    for i in range(winner_count):
-        check_strings.append('')
+    length = len(game_matrix[0])
+    width = len(game_matrix)
+    winner_count = width
+    diagonals_number = 2  # default value
+    if width > length:
+        winner_count = length
+        diagonals_number = (width - length + 1) * 2
+    else:
+        diagonals_number = (length - width + 1) * 2
 
     # check if winner is on horizontal line
-    for array in game_matrix:
-        if array.count(char) == winner_count:
-            return char
-
+    if check_winner_by_line(winner_count, char, game_matrix) == char:
+        return char
     # check if winner is on vertical line
     rotated_game_matrix = rotate_matrix(game_matrix)
-    for index in range(len(rotated_game_matrix)):
-        for element in rotated_game_matrix[index]:
-            check_strings[index] += element
+    if check_winner_by_line(winner_count, char, rotated_game_matrix) == char:
+        return char
+
+    counter = winner_count
+    if width < diagonals_number // 2 or length < diagonals_number // 2:
+        counter = diagonals_number // 2
+
+    counts = []  # for diagonals
+    for _ in range(diagonals_number * 2):
+        counts.append('')
+    return check_winner_by_diagonal(counts, counter, winner_count, char, game_matrix)
+
+
+def check_winner_by_diagonal(counts, count, winner_count, char, game_matrix):
+    # check if winner is by diagonal (from left to right corner)
+    for index in range(count):
+        for index_1 in range(index, count + index):
+            counts[index] += game_matrix[index_1][index]
+
+    # check if winner is by diagonal (from right to left corner)
+    for index in reversed(range(count)):
+        for index_1 in range(count - index - 1, count * 2 - index - 1):
+            counts[count * 2 - index - 1] += game_matrix[index_1][index]
+
+    rotated_counts = rotate_matrix(counts)
+    for diagonal in rotated_counts:
         check_winner_str = char * winner_count
-        if check_strings[index].__contains__(check_winner_str):
+        diagonal_str = ''
+        for elem in diagonal:
+            diagonal_str += elem
+        if check_winner_str in diagonal_str:
             return char
 
-    counts = []  # four diagonals (diagonal is line that begins in the corner)
-    for i in range(4):
-        counts.append(0)
 
-    # check if winner is by diagonal
-    for index in range(winner_count):
-        if game_matrix[index][index] == char:  # 1
-            counts[0] += 1
-        if game_matrix[index][winner_count - 1 - index] == char:  # 2
-            counts[1] += 1
-        if game_matrix[index + 1][index] == char:  # 3
-            counts[2] += 1
-        if game_matrix[index + 1][winner_count - 1 - index] == char:  # 4
-            counts[3] += 1
+def check_winner_by_line(winner_count, char, game_matrix):
+    length = len(game_matrix)
+    check_strings = []
+    for i in range(length):
+        check_strings.append('')
 
-    for count in counts:
-        if count == winner_count:
+    for index in range(length):
+        for element in game_matrix[index]:
+            check_strings[index] += element
+        check_winner_str = char * winner_count
+        if check_winner_str in check_strings[index]:
             return char
 
 
@@ -46,14 +67,14 @@ def rotate_matrix(game_matrix):
 
 
 def is_winner(gamer, mark, game_matrix):
-    try:
+    if 0 <= gamer[0] < len(game_matrix[0]) and 0 <= gamer[1] < len(game_matrix):
         if game_matrix[gamer[0]][gamer[1]] == '.':
             game_matrix[gamer[0]][gamer[1]] = mark
             draw_current_game(game_matrix)
             return determine_winner(mark, game_matrix)
         else:
             print('Error! This field is not empty!')
-    except IndexError:
+    else:
         print('IndexError! List index out of range.')
 
 
