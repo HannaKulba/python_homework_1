ef8b08b8ef4f1d17a588419a1a6d5b35ceeecf6f
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-02-12 17:44:43 +0300

    update solution

--- homework_2/6_list_a.py
+++ homework_2/6_list_a.py
@@ -5,16 +5,11 @@
 # out [1, 2, 3, 4, 5, 6, 7]
 ################################################################################
 
-def get_list(matrix, result_list=None):
-    if result_list is None:
-        result_list = []
+def get_list(matrix):
+    result_list = []
     for element in matrix:
         if type(element) is list:
-            for elem in element:
-                if type(elem) is list:
-                    get_list(elem, result_list)
-                else:
-                    result_list.append(elem)
+            result_list += get_list(element)
         else:
             result_list.append(element)
     return result_list
