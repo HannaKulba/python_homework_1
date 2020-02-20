7d0a50f5e94d0aacd5ca2a2d152ea21056502243
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-02-20 12:49:54 +0300

    update file names

---
+++ homework_4/2_thing_2.py
@@ -0,0 +1,10 @@
+class Thing2:
+    letters = ''
+
+    def __init__(self):
+        pass
+
+
+example = Thing2()
+example.letters = 'abc'
+print(example.letters)
--- homework_4/Thing2.py
+++
@@ -1,7 +0,0 @@
-class Thing2:
-    def __init__(self, letters):
-        self.letters = letters
-
-
-example = Thing2('abc')
-print(example.letters)
