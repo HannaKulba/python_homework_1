6f61d88d336158a457748ce5116b9ffee2c01e14
PC002-SG-KULBA-\user <anna.mirraza@gmail.com>
2020-02-20 10:17:52 +0300

    add done tasks hw #4

---
+++ homework_4/Thing.py
@@ -0,0 +1,9 @@
+class Thing:
+    def __init__(self):
+        pass
+
+
+print(Thing)  # <class '__main__.Thing'>
+
+example = Thing()
+print(example)  # <__main__.Thing object at 0x000001B9FD3E61F0>
---
+++ homework_4/Thing2.py
@@ -0,0 +1,7 @@
+class Thing2:
+    def __init__(self, letters):
+        self.letters = letters
+
+
+example = Thing2('abc')
+print(example.letters)
